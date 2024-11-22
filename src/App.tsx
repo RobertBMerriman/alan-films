81f40406ff8a4ab0dbf337b89c510cccd71a55b9
Robert Merriman <robertbmerriman@gmail.com>
2024-11-22 15:08:42 +0000

    React query init

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import './App.css'
 
-import viteLogo from '/vite.svg'
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
+import { ThemeProvider } from '@/components/ui/theme-provider.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = new QueryClient()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-400">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,10 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { ThemeProvider } from '@/components/ui/theme-provider.tsx'
-
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
