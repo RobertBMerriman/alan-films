a80018086d7baac7f493c32fa6152bea80f607d5
Robert Merriman <robertbmerriman@gmail.com>
2024-11-22 15:21:42 +0000

    Tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import './App.css'
 
 import { ThemeProvider } from '@/components/ui/theme-provider.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import { routeTree } from './routeTree.gen'
+
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 function App() {
-  const queryClient = new QueryClient()
-
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ThemeProvider>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
