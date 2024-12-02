72a2e8ccffdaafea47c8a4c6e7af1969c87edd86
Robert Merriman <robertbmerriman@gmail.com>
2024-12-02 19:55:50 +0000

    Filter by who's in! (#1)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import FilmDetail from '@/components/films/FilmDetail'
+import UserFilters from '@/components/films/UserFilters'
 import { P } from '@/components/ui/typeography'
 import { useAuthedUser, useGetFilms, usePublicUsers } from '@/services/supabase'
 import { createFileRoute } from '@tanstack/react-router'
@@ -12,16 +15,29 @@ function RouteComponent() {
   const { data: films, isSuccess: isFilmsSuccess, isLoading: isFilmsLoading } = useGetFilms()
   const { data: allUsers, isSuccess: isUsersSuccess, isLoading: isUsersLoading } = usePublicUsers()
 
+  const [userIds, setUserIds] = useState<string[]>([])
+
   if (isFilmsLoading) return <P>Loading...</P>
   if (isUsersLoading) return <P>Loading...</P>
   if (!isFilmsSuccess) return <P>Error</P>
   if (!isUsersSuccess) return <P>Error</P>
 
+  const filteredFilms = films.filter((film) => {
+    const interestedIds = film.users_films
+      .filter((uf) => uf.state === 'interested')
+      .map((uf) => uf.user_id)
+
+    return !interestedIds.some((id) => userIds.includes(id))
+  })
+
   return (
     <>
       {isAuthError && <p className="text-center">Login to make changes!</p>}
+
+      <UserFilters userIds={userIds} setUserIds={setUserIds} />
+
       <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
-        {films.map((film) => (
+        {filteredFilms.map((film) => (
           <FilmDetail key={film.id} film={film} users={allUsers} />
         ))}
       </div>
